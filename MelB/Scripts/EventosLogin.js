681ea0f9402fa8c14a53c1d9fced21a8eae42a91
X3R0XP3R <riveramoreira04@gmail.com>
2018-05-05 22:42:53 -0600

    Login

--- MelB/Scripts/EventosLogin.js
+++ MelB/Scripts/EventosLogin.js
@@ -1,5 +1,35 @@
+var FormularioActivo = "Login";
+
 $(document).ready(function()
+{
+	InicializacionControles();
+	InicializacionEventos();
+});
+
+
+function InicializacionControles()
 {
 	$('.selectpicker').selectpicker();
 	$('.selectpicker').selectpicker('refresh');
-});
+}
+
+function InicializacionEventos()
+{
+	$('#RecuperarContraseña').click(function(event)
+	{
+		if(FormularioActivo == "Login")
+		{			
+			$('#FormularioLogin').hide(300);
+			$('#FormularioRecuperarContraseña').show(300);
+			$('#RecuperarContraseña').text('Regresar');
+			FormularioActivo = "Recuperación";
+		}
+		else
+		{
+			$('#FormularioRecuperarContraseña').hide(300);
+			$('#FormularioLogin').show(300);
+			$('#RecuperarContraseña').text('¿Olvidaste la contraseña?');
+			FormularioActivo = "Login";
+		}
+	});
+}
