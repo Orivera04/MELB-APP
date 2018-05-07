5d4bbb8f74a8df218c830c0b69400d3d3ad18b01
X3R0XP3R <riveramoreira04@gmail.com>
2018-05-06 23:53:52 -0600

    d
    f

--- WEBAPP_MELB/WEBAPP_MELB/obj/Release/Package/PackageTmp/Scripts/EventosLogin.js
+++ WEBAPP_MELB/WEBAPP_MELB/obj/Release/Package/PackageTmp/Scripts/EventosLogin.js
@@ -42,6 +42,61 @@ function InicializacionEventos()
         }
     });
 
+    $('#BotonEnviarContraseña').click(function (event)
+    {
+        if ($('#Correo').val() != "") {
+            if (ValidarCorreo($('#Correo').val()) == true) {
+                swal({ title: 'Enviando', text: 'Se esta enviando el mensaje espere por favor', type: 'info', allowOutsideClick: false });
+                swal.showLoading();
+                $.ajax
+                    ({
+                        url: 'http://melbws.azurewebsites.net/api/Credenciales?Correo=' + $('#Correo').val() + '&Modulo=' + $('#ModuloContraseña').val(),
+                        type: 'GET',
+                        success: function (Resultado) {
+                            var Resultado = JSON.parse(Resultado);
+                            if (Resultado.Exito == true) {
+                                swal
+                                    ({
+                                        title: "Exito",
+                                        text: "Las credenciales se enviaron al correo ingresado",
+                                        type: "success",
+                                    });
+                            }
+                            else {
+                                swal
+                                    ({
+                                        title: Resultado.Mensaje_Cabecera,
+                                        text: Resultado.Mensaje_Usuario,
+                                        type: "warning",
+                                    });
+                            }
+                        },
+                        error: function (Error) {
+                            swal
+                                ({
+                                    title: "Error",
+                                    text: "Ocurrio un error al intentar enviar el correo",
+                                    type: "error",
+                                });
+                        }
+                    })
+            }
+            else
+            {
+                swal
+                    ({
+                        title: "Aviso",
+                        text: "El correo no tiene un formato correcto",
+                        type: "warning",
+                    });
+            }
+        }
+        else
+        {
+            swal("Aviso", "Tiene que introducir un correo", "warning");
+        }
+    });
+
     $('#Usuario').keypress(function (event)
     {
         if (event.which == 13)
@@ -59,4 +114,12 @@ function InicializacionEventos()
             $("#BotonLogin").click();
         }
     });
+}
+
+/* Funciones de soporte */
+
+function ValidarCorreo(Correo)
+{
+    var RegXP = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return RegXP.test(Correo);
 }
